94a09667a76fb15137601eb74687a917d9ce22ea
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2025-04-02 16:51:27 +0000

    Modified for two nodes

--- distexperiments/experiments/config.py
+++ distexperiments/experiments/config.py
@@ -6,8 +6,8 @@ SERVER_PORT = 20005
 
 server_list = ServerList(
     # fill in
-    Server('node08', 'c08.lab.dm.informatik.tu-darmstadt.de', SERVER_PORT, ibIp='172.18.94.80', sibIP='172.18.94.81', ssdPath="/dev/md0"),
-    Server('node07', 'c07.lab.dm.informatik.tu-darmstadt.de', SERVER_PORT, ibIp='172.18.94.70', sibIP='172.18.94.71', ssdPath="/dev/md0 "),
+    Server('node0', 'sm110p-10s10608.wisc.cloudlab.us', SERVER_PORT, ibIp='10.10.1.1', sibIP='10.10.1.1', ssdPath="/dev/md0"),
+    Server('node1', 'sm110p-10s10607.wisc.cloudlab.us', SERVER_PORT, ibIp='10.10.1.2', sibIP='10.10.1.2', ssdPath="/dev/md0 "),
     Server('node06', 'c06.lab.dm.informatik.tu-darmstadt.de', SERVER_PORT, ibIp='172.18.94.60', sibIP='172.18.94.61', ssdPath="/dev/md0"),
     
     Server('node04', 'c04.lab.dm.informatik.tu-darmstadt.de', SERVER_PORT, ibIp='172.18.94.40', sibIP='172.18.94.41', ssdPath="/dev/md127"),
--- distexperiments/experiments/optimistic_scaleout.py
+++ distexperiments/experiments/optimistic_scaleout.py
@@ -1,10 +1,15 @@
+import sys
+
 import config
 from distexprunner import *
 
-NUMBER_NODES = 5
+NUMBER_NODES = 2
+BIN_DIR = "/proj/sandstorm-PG0/ashfaq/repos/RDMA_synchronization/build/frontend"
+IF_NAME = "ens2f0np0"
 
 parameter_grid = ParameterGrid(
-    worker=[1,2,4,8,16,32,64,128,256,512,1024,2048],
+    # worker=[1,2,4,8,16,32,64,128,256,512,1024,2048],
+    worker=[1,2],
     locks=[200000],
     options=["-CRC", "-farm" , "-versioning", "-broken", "-pessimistic"],
     footer=["-nofooter","-footer"],
@@ -13,23 +18,23 @@ parameter_grid = ParameterGrid(
 )
 
 
-@reg_exp(servers=config.server_list[:NUMBER_NODES])
-def compile(servers):
-    servers.cd("/home/tziegler/rdma_synchronization/build/")
-    cmake_cmd = f'cmake -D CMAKE_C_COMPILER=gcc-10 -D CMAKE_CXX_COMPILER=g++-10 -DCMAKE_BUILD_TYPE=Release ..'
-    procs = [s.run_cmd(cmake_cmd) for s in servers]
-    assert(all(p.wait() == 0 for p in procs))
-
-    make_cmd = f'sudo make -j'
-    procs = [s.run_cmd(make_cmd) for s in servers]
-    assert(all(p.wait() == 0 for p in procs))
+# @reg_exp(servers=config.server_list[:NUMBER_NODES])
+# def compile(servers):
+#     servers.cd("/home/tziegler/rdma_synchronization/build/")
+#     cmake_cmd = f'cmake -D CMAKE_C_COMPILER=gcc-11 -D CMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=Release ..'
+#     procs = [s.run_cmd(cmake_cmd) for s in servers]
+#     assert(all(p.wait() == 0 for p in procs))
+#
+#     make_cmd = f'sudo make -j'
+#     procs = [s.run_cmd(make_cmd) for s in servers]
+#     assert(all(p.wait() == 0 for p in procs))
     
 
 @reg_exp(servers=config.server_list[:NUMBER_NODES], params=parameter_grid, raise_on_rc=True, max_restarts=1)
 def locking_benchmark(servers, worker,locks, options, footer, blocks):    
-    servers.cd("/home/tziegler/rdma_synchronization/build/frontend")        
+    servers.cd(BIN_DIR)        
     cmds = []
-    cmd = f'numactl --membind=0 --cpunodebind=0 sudo ip netns exec ib0  ./optmistic_benchmark -ownIp={servers[0].ibIp} -storage_node -worker={worker} -lock_count={locks} -dramGB=10'
+    cmd = f'numactl --membind=0 --cpunodebind=0 sudo ip netns exec {IF_NAME} ./optmistic_benchmark -ownIp={servers[0].ibIp} -storage_node -worker={worker} -lock_count={locks} -dramGB=10'
     cmds += [servers[0].run_cmd(cmd)]
 
     work = worker
@@ -39,7 +44,7 @@ def locking_benchmark(servers, worker,locks, options, footer, blocks):
         numberNodes=4
     
     for i in range(1, numberNodes+1):
-        cmd = f'numactl --membind=0 sudo ip netns exec ib0  ./optmistic_benchmark -ownIp={servers[i].ibIp} -all_worker={worker} -worker={work} -csvFile="optimistic_benchmark_scaleout_new.csv" -run_for_seconds=30 -tag={worker} -nopinThreads -lock_count={locks} {options} {footer} -block_size={blocks}'
+        cmd = f'numactl --membind=0 sudo ip netns exec {IF_NAME} ./optmistic_benchmark -ownIp={servers[i].ibIp} -all_worker={worker} -worker={work} -csvFile="optimistic_benchmark_scaleout_new.csv" -run_for_seconds=30 -tag={worker} -nopinThreads -lock_count={locks} {options} {footer} -block_size={blocks}'
         cmds += [servers[i].run_cmd(cmd)]
         
     if not all(cmd.wait() == 0 for cmd in cmds):
