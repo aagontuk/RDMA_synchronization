8d10c9b1f92d94a8edc3596031b99d4b22491730
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2025-07-04 01:01:19 +0000

    hugepage setup

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 # Installation script for Ubuntu 22.04 #
 ########################################
 
+N_HUGE_PAGE=10240
+
+# Setup hugepages
+# echo "${N_HUGE_PAGE}" | sudo tee /proc/sys/vm/nr_hugepages
+
 # Install necessary packages
 sudo apt update
 sudo apt install libnuma-dev libaio-dev
