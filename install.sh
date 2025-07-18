5f5cc1ac1358ee07fffa47a9b8f92a9360141c76
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2025-07-17 23:28:36 -0600

    updated setup script

--- install.sh
+++ install.sh
@@ -10,19 +10,36 @@ n_huge=$(cat /proc/meminfo | grep -i HugePages_Total | awk '{print $2}')
 s_huge=$(cat /proc/meminfo | grep -i Hugepagesize | awk '{print $2,$3}')
 
 # Setup hugepages
-if [ ! $n_huge -eq $N_HUGE_PAGE ]; then
-  echo "ERROR: No hugepages found"
-  echo "Setting up hugepages"
-  echo "${N_HUGE_PAGE}" | sudo tee /proc/sys/vm/nr_hugepages
+function setup_hugepages() {
+  if [ ! $n_huge -eq $N_HUGE_PAGE ]; then
+    echo "ERROR: No hugepages found"
+    echo "Setting up hugepages"
+    echo "${N_HUGE_PAGE}" | sudo tee /proc/sys/vm/nr_hugepages
+  else
+    echo "Found ${n_huge} pages of size ${s_huge}"
+  fi
+}
+
+function build() {
+  # Install necessary packages
+  sudo apt update
+  sudo apt install libnuma-dev libaio-dev
+
+  mkdir build
+  cd build
+  cmake ..
+  make -j$(nproc)
+}
+
+# Command line arguments
+if [ "$1" == "hugepages" ]; then
+  setup_hugepages
+elif [ "$1" == "build" ]; then
+  build
+elif [ "$1" == "all" ]; then
+  setup_hugepages
+  build
 else
-  echo "Found ${n_huge} pages of size ${s_huge}"
+  echo "Usage: $0 {hugepages | build | all}"
+  exit 1
 fi
-
-# Install necessary packages
-sudo apt update
-sudo apt install libnuma-dev libaio-dev
-
-mkdir build
-cd build
-cmake ..
-make -j$(nproc)
