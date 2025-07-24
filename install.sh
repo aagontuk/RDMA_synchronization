85f5a7e4caf64a27d2914e65d9a3b2f0860fd245
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2025-07-24 08:48:28 +0000

    updated setup file

--- install.sh
+++ install.sh
@@ -20,6 +20,31 @@ function setup_hugepages() {
   fi
 }
 
+function system_setup() {
+  vendor=$(grep -m 1 "vendor_id" /proc/cpuinfo)
+
+  sudo apt update
+  sudo apt install -y linux-tools-common linux-tools-`uname -r` htop
+
+  # Install plot packages
+  sudo apt install -y python3-pip
+  pip install matplotlib
+
+  if [[ "$vendor" == *"GenuineIntel"* ]]; then
+    # Set scaling governor to performance
+    sudo cpupower frequency-set --governor performance
+
+    # Disable turbo
+    echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+  fi
+
+  # Turn off SMT
+  if [ $(cat /sys/devices/system/cpu/smt/control) = "on" ]; then
+    echo "Disabling SMT (Simultaneous Multithreading)..."
+    echo off | sudo tee /sys/devices/system/cpu/smt/control
+  fi
+}
+
 function build() {
   # Install necessary packages
   sudo apt update
@@ -36,10 +61,13 @@ if [ "$1" == "hugepages" ]; then
   setup_hugepages
 elif [ "$1" == "build" ]; then
   build
+elif [ "$1" == "setup" ]; then
+  system_setup
 elif [ "$1" == "all" ]; then
+  system_setup
   setup_hugepages
   build
 else
-  echo "Usage: $0 {hugepages | build | all}"
+  echo "Usage: $0 {setup | hugepages | build | all}"
   exit 1
 fi
