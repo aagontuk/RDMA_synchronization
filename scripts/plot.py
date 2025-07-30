76343b406a63c6fb6b76c8e946bc4210e7ff0bd1
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2025-07-30 23:20:27 +0000

    updated plot script

--- scripts/plot.py
+++ scripts/plot.py
@@ -43,6 +43,8 @@ plt.rc('figure', titlesize=BIGGER_SIZE)
 
 X_COLUMN_SIZES='size'
 X_LBL_SIZES='Sizes'
+X_COLUMN_THREADS='threads'
+X_LBL_THREADS='Threads'
 Y_COLUMN='throughput'
 Y_LBL='Mop/s'
 LABELS = {"pessimistic": "pessimistic_batch_32", "pessimistic_b1": "pessimistic", "broken": "broken", "rc": "rc_batch_32", "rcopt": "rc_2QP_batch_16"}
@@ -67,11 +69,11 @@ def plot_sizes_vs_tput(df, machine, threads, out_dir):
 
     plt.xlabel(X_LBL_SIZES)
     plt.ylabel(Y_LBL)
-    plt.title('Throughput vs Sizes for {} with {} threads'.format(machine, threads))
+    plt.title('Sizes vs Throughput for {} with {} threads'.format(machine, threads))
 
 
     ax.set_xscale('log', base=2)
-    plt.xticks(rotation=90)
+    # plt.xticks(rotation=90)
     plt.xticks(ticks=sizes, labels=sizes)
 
     y_lim = y_max * 1.1
@@ -84,23 +86,71 @@ def plot_sizes_vs_tput(df, machine, threads, out_dir):
     for file in file_paths:
         fig.savefig(file, dpi=300)
 
+def plot_threads_vs_tput(df, machine, size, out_dir):
+    file_paths = []
+    file_paths.append(os.path.join(out_dir, "sizes_vs_tput_m{}_s{}.pdf".format(machine, size)))
+    file_paths.append(os.path.join(out_dir, "sizes_vs_tput_m{}_s{}.png".format(machine, size)))
+
+    fig, ax = plt.subplots(figsize=(PLOT_WIDTH, PLOT_HEIGHT))
+
+    # Find all the unique names from the 'bench' columns
+    bench_names = df['bench'].unique()
+    threads = df['threads'].unique()
+
+    y_max = 0
+    for name in bench_names:
+        df_filtered = df[(df['bench'] == name) & (df['size'] == size)]
+        df_filtered.plot(kind='line', ax=ax, x=X_COLUMN_THREADS, y=Y_COLUMN, marker='o', color=COLORS[name], label= LABELS[name])
+        y_max = max(y_max, df_filtered[Y_COLUMN].max())
+
+    plt.xlabel(X_LBL_THREADS)
+    plt.ylabel(Y_LBL)
+    plt.title('Threads vs Throughput for {} with {} size'.format(machine, size))
+
+
+    ax.set_xscale('log', base=2)
+    # plt.xticks(rotation=90)
+    plt.xticks(ticks=threads, labels=threads)
+
+    y_lim = y_max * 1.1
+
+    ax.set_ylim(bottom=0, top=y_lim)
+    ax.set_xlim(left=threads.min())
+
+    fig = plt.gcf()
+    fig.tight_layout()
+    for file in file_paths:
+        fig.savefig(file, dpi=300)
+
 def main():
     parser = argparse.ArgumentParser(description='Plot latency and throughput graphs.')
     parser.add_argument('data_file', type=str, help='Path to the data file containing latency and throughput data')
     parser.add_argument('--output_dir', type=str, default='.', help='Directory to save the output plots (default: current directory)')
     parser.add_argument('--machine', type=str, default='sm110p', help='CloudLab Machine name (default: sm110p)')
+    parser.add_argument('--type', type=str, default='sizes', help='Plot type ["sizes", "threads"] (default: "sizes")')
     parser.add_argument('--threads', type=int, default=1, help='Number of threads to plot (default: 1)')
+    parser.add_argument('--size', type=int, default=64, help='Size to plot (default: 64)')
 
     args = parser.parse_args()
     
     data_file = args.data_file
     output_dir = args.output_dir
     machine = args.machine
+    plot_type = args.type
     threads = args.threads
+    size = args.size
 
     df = pd.read_csv(data_file)
 
-    plot_sizes_vs_tput(df, machine, threads, output_dir)
+    if plot_type == 'sizes':
+        print("Plotting sizes vs throughput for machine: {}, threads: {}".format(machine, threads))
+        plot_sizes_vs_tput(df, machine, threads, output_dir)
+    elif plot_type == 'threads':
+        print("Plotting threads vs throughput for machine: {}, size: {}".format(machine, size))
+        plot_threads_vs_tput(df, machine, size, output_dir)
+    else:
+        print("Invalid plot type. Use 'sizes' or 'threads'.")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
