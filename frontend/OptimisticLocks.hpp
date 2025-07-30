853c70b04a5406b7f0818ec17bde722a9eaf5e70
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2025-07-30 20:55:22 +0000

    Use batch size for others twice of RC_2QP

--- frontend/OptimisticLocks.hpp
+++ frontend/OptimisticLocks.hpp
@@ -28,7 +28,7 @@ static constexpr uint64_t UNLOCKED = 0;
 static constexpr uint64_t MASKED_SHARED_LOCKS = 0x1000000000000000;
 static constexpr uint64_t SHARED_UNLOCK_TO_BE_ADDED = 0xFFFFFFFFFFFFFFFF;
 
-static constexpr int RC_BATCH_SIZE =  32;
+static constexpr int RC_BATCH_SIZE =  16;
 // -------------------------------------------------------------------------------------
 // Protected region is just a memory buffer
 // FaRM Footer layout:[V]...[V]...[V]...[L]
@@ -359,12 +359,12 @@ struct OptimisticLock {
           }
         }
         else {
-          if (opnum != 0 && ((opnum + 1) % RC_BATCH_SIZE) == 0) {
+          if (opnum != 0 && ((opnum + 1) % (RC_BATCH_SIZE * 2)) == 0) {
             int comp{0};
 
             int tot_comp{0};
-            int tot_expected{RC_BATCH_SIZE};
-            ibv_wc wcReturn[RC_BATCH_SIZE];
+            int tot_expected{RC_BATCH_SIZE * 2};
+            ibv_wc wcReturn[RC_BATCH_SIZE * 2];
             while (tot_comp != tot_expected) {
               _mm_pause();
               auto expected = tot_expected - tot_comp;
@@ -523,12 +523,12 @@ struct ExclusiveLock {
             comp = rdma::pollCompletion(rctx.id->qp->send_cq, 1, &wcReturn);
          }
          */
-        if (opnum != 0 && ((opnum + 1) % RC_BATCH_SIZE) == 0) {
+        if (opnum != 0 && ((opnum + 1) % (RC_BATCH_SIZE * 2)) == 0) {
           int comp{0};
 
           int tot_comp{0};
-          int tot_expected{RC_BATCH_SIZE};
-          ibv_wc wcReturn[RC_BATCH_SIZE];
+          int tot_expected{RC_BATCH_SIZE * 2};
+          ibv_wc wcReturn[RC_BATCH_SIZE * 2];
           while (tot_comp != tot_expected) {
             _mm_pause();
             auto expected = tot_expected - tot_comp;
